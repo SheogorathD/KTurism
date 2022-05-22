ac73f4ce280599e386ce974fa4d25bd31239feac
Jun Dev <jun.meathammer@gmail.com>
2022-05-22 13:47:51 +0300

    Add templates, rewrite pages, rewrite style.css

---
+++ inc/templates/about-us.php
@@ -0,0 +1,48 @@
+<section class="about">
+   <div class="container">
+      <h2 class="title">Про нас</h2>
+      <ul class="about__list">
+         <li class="about__item">
+            <div class="about__item-box"> 
+                <img src="<?php echo get_template_directory_uri( ); ?>../../media/img/about-us/Orlenko.jpg" alt="Orlenko">
+                 <h3>Орленко Олена Владиславівна</h3>
+                <p>Завідувачка кафедри готельно-ресторанного та туристичного бізнесу, професорка, докторка економічних наук.</p>
+             </div>
+         </li>
+         <li class="about__item">
+            <div class="about__item-box">
+                <img src="<?php echo get_template_directory_uri( ); ?>../../media/img/about-us/Yarovuy.jpg" alt="Yarovuy">
+                <h3>Яровий Вадим Федорович</h3>
+                <p>Доцент, доктор економічних наук.</p>
+            </div>
+         </li>
+         <li class="about__item">
+            <div class="about__item-box">
+                <img src="<?php echo get_template_directory_uri( ); ?>../../media/img/about-us/Makienko.jpg" alt="Makienko">
+                <h3>Макієнко Олексій Анатолійович</h3>
+                <p>Доцент, кандидат історичних наук.</p>
+            </div>
+         </li>
+         <li class="about__item">
+            <div class="about__item-box">
+               <img src="<?php echo get_template_directory_uri( ); ?>../../media/img/about-us/IMG_9687.jpg" alt="Lemeshchuk">
+               <h3>Лемещук Олександр Ігорович</h3>
+               <p>Менеджмент ІТ проєкту.</p>
+            </div>
+         </li>
+         <li class="about__item">
+            <div class="about__item-box">
+               <img src="<?php echo get_template_directory_uri( ); ?>../../media/img/about-us/Shcherbyna.jpg" alt="Shcherbyna">
+               <h3>Щербина Володимир Володимирович</h3>
+               <p>ІТ розробник.</p></div>
+         </li>
+         <li class="about__item">
+            <div class="about__item-box">
+                  <img src="<?php echo get_template_directory_uri( ); ?>../../media/img/about-us/Zhukov.jpg" alt="Zhukov">
+                  <h3>Жуков Андрій Сергійович</h3>
+                  <p>ІТ розробник.</p>
+            </div>
+         </li>
+      </ul>
+   </div>
+</section>
---
+++ inc/templates/tour_list.php
@@ -0,0 +1,56 @@
+<section class="tour-list">
+    <div class="container">
+        <h2 class="title">Маршрут водно-пішохідної екскурсії "Мандри загубленим світом"</h2>
+        <p class="description">Від Національного природного парку «Нижньодніпровський»</p>
+        <ul class="tour__list">
+            <li class="tour__list-item">
+                <div class="tour__item-box">
+                    <img src="<?php echo get_template_directory_uri( ); ?>../../media/img/tour/Кемпінг Козак.jpg" alt="kemping_kozak"></img>
+                    <h3>Кемпінг "Козак"</h3>
+                </div>
+            </li>
+            <li class="tour__list-item">
+                <div class="tour__item-box">
+                    <img src="<?php echo get_template_directory_uri( ); ?>../../media/img/tour/Велетні загубленого світу.jpg" alt="zagubleniy_svit"></img>
+                    <h3>Велетні загубленого світу</h3>
+                </div>
+            </li>
+            <li class="tour__list-item">
+                <div class="tour__item-box">
+                    <img src="<?php echo get_template_directory_uri( ); ?>../../media/img/tour/Вхід до загубленого світу Дніпровської Гілеї.jpg" alt="dniprovska_gileya"></img>
+                    <h3>Вхід до загубленого світу Дніпровської Гілеї</h3>
+                </div>
+            </li>
+            <li class="tour__list-item">
+                <div class="tour__item-box">
+                    <img src="<?php echo get_template_directory_uri( ); ?>../../media/img/tour/Загублений світ_Річка Хмільник.jpg" alt="hmilnyk"></img>
+                    <h3>Загублений світ. Річка Хмільник</h3>
+                </div>
+            </li>
+            <li class="tour__list-item">
+                <div class="tour__item-box">
+                    <img src="<?php echo get_template_directory_uri( ); ?>../../media/img/tour/Зарості латаття.jpg" alt="zarosti_latattya"></img>
+                    <h3>Зарості латаття</h3>
+                </div>
+            </li>
+            <li class="tour__list-item">
+                <div class="tour__item-box">
+                    <img src="<?php echo get_template_directory_uri( ); ?>../../media/img/tour/Річка Козак.jpg" alt="richka_kozak"></img>
+                    <h3>Річка Козак</h3>
+                </div>
+            </li>
+            <li class="tour__list-item">
+                <div class="tour__item-box">
+                    <img src="<?php echo get_template_directory_uri( ); ?>../../media/img/tour/bober.jpg" alt="bober"></img>
+                    <h3>Бобер європейський</h3>
+                </div>
+            </li>
+            <li class="tour__list-item">
+                <div class="tour__item-box">
+                    <img src="<?php echo get_template_directory_uri( ); ?>../../media/img/tour/Робота бобрів.jpg" alt="robota_bobriv"></img>
+                    <h3>Робота бобрів</h3>
+                </div>
+            </li>
+        </ul>
+    </div>
+</section>
