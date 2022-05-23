7ee13740c19afafa4c4bc1a5995ad42958c4c4c5
SheogorathD <aknojlogiya@gmail.com>
2022-05-23 22:47:04 +0300

    hot-fix

--- functions.php
+++ functions.php
@@ -22,38 +22,14 @@ register_nav_menus(
 		'mobile-menu' => 'Mobile Menu Location',
 	)
 );	
-	/**
-	 * Возвращает все URL картинок первой галереи из указанного текста записи.
-	 * Вернёт результат, если галлерея есть и вставлена как блок (Гутенберг).
-	 */
-function get_post_block_gallery_images( $post = 0 ) {
-	$post = get_post( $post );
 
-	// Запись не нашлась - возвращаем пустой массив
-	if ( ! is_a( $post, 'WP_Post' ) ) {
-		return [];
-	}
-
-	// Блок "Галерея" не нашелся - возвращаем пустой массив
-	if ( ! has_block( 'gallery', $post->post_content ) ) {
-		return [];
-	}
-
-	// Ищем все блоки в контенте
-	$post_blocks = parse_blocks( $post->post_content );
-
-	// Перебираем все блоки в поисках нужного
-	foreach ( $post_blocks as $block ) {
-		// Ищем блок "Галерея" с переданными ID изображений
-		if ( $block['blockName'] === 'core/gallery' && ! empty( $block['attrs']['ids'] ) ) {
-			return array_map( function ( $image_id ) {
-				// Получаем ссылку на оригинал изображения
-				return wp_get_attachment_image_url( $image_id, 'full' );
-			}, $block['attrs']['ids'] );
-		}
-	}
-
-	// Если блок "Галерея" не нашелся - возвращаем пустой массив
-	return [];
+function vid( $name ){
+	echo get_template_directory_uri() . '/assets/video/' . $name;
+}
+function taudio( $name ){
+	echo get_template_directory_uri() . '/assets/audio/' . $name;
+}
+function oimage( $name ){
+	echo get_template_directory_uri() . '/assets/img/object/' . $name;
 }
 ?>
